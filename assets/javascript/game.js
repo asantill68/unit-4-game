9dbcd3c7ff6a6a52dcd00a2d1447401bc94b63f9
Alex Santillan <asantill68@yahoo.com>
2019-10-18 23:50:00 -0500

    JS almost completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,118 @@
-//Create and reset variables for wins, losses and score.
-var wins = 0;
-var losses = 0;
+//Crystal Variables
+var crystal = {
+    red:
+    {
+        name:  "red",
+        value: 0,
+    },
+    blue:
+    {
+        name:  "blue",
+        value: 0,
+    },
+    gold:
+    {
+        name:  "gold",
+        value: 0,
+    },
+    green:
+    {
+        name:  "green",
+        value: 0,
+    },
+};
+
+//Scores to track
+var score   = 0;
+var target  = 0;
+//Generates random target between 19 and 120
+//var target = Math.floor(Math.random() * 101) + 19;
+//$("#target").html(target);
+
+//Create and reset variables for wins and losses.
+var wins    = 0;
+var losses  = 0;
+
+//Create an array and assign four values <19
+//Use a for-loop to randomely assign values between 1 and 19 to each spot on array
+//var crystalVal = [1, 3, 10, 15];
+
+//Functions
+
+//Reset the current score
 var score = 0;
 
-//Generates random target between 19 and 120
-var target = Math.floor(Math.random() * 101) + 19;
+//General random number generator with two limits
+var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var startGame = function() {
+//Set a new target score (between 19 and 120)
+var target = getRandom (19, 120);
+
+//Set different values for each of the crystals (between 1 and 12)
+crystal.red.value   = getRandom (1, 12);
+crystal.blue.value  = getRandom (1, 12);
+crystal.gold.value  = getRandom (1, 12);
+crystal.green.value = getRandom (1, 12);
+
+//Change the html to refelect the changes
 $("#target").html(target);
 
-//Create an array and use a for-loop to randomely assign values between 1 and 19 to each spot on array
-var crystalVal = [1, 3, 10, 15];
-
-//Target wins, losses and score in the html
-$("#wins").html("Wins: " + wins);
-$("#losses").html("Losses: " + losses);
-$("#score").html(score);
 
 
-//Assign on.Click to images
+
+//test
+//console.log("-------------------------------------");
+//console.log("target score " + target);
+//console.log("red: " + crystal.red.value + " | blue : " + crystal.blue.value + " | gold: " + crystal.gold.value + " | green: " + crystal.green.value);
+//console.log ("-------------------------------------")
+};
+//Starts the game
+startGame ();
+
+//Function to add crystal values
+var addValue = function(crystal){
+    score = score + crystal.value;
+    //test
+    //console.log("Your score " + score);
+
+    //Update the score
+    $("#score").html(score);
+
+    //Call checkWin
+    checkWin();
+}
+
+//Function to check Wins
+var checkWin = function() {
+    if(score > target){
+        alert("Sorry.  You lost!");
+        //Update losses
+        losses ++;
+        //Update html
+        $("#losses").html("Losses: " + losses);
+        }
+    else if(score == target) {
+    alert("Congratulations!!!  You Won");
+    //Update wins
+    wins ++;
+    //Update html
+    $("#wins").html("Wins: " + wins);
+    }
+};
+
+//Assign on.Click and value to images
 $("#red-crystal").on("click", function () {
-    var redVal = crystalVal[0];
-    alert("Your current score is = " + redVal);
+    addValue(crystal.red);
 });
 $("#blue-crystal").on("click", function () {
-    var blueVal = crystalVal[1];
-    alert("Your current score is = " + blueVal);    
+    addValue(crystal.blue);
 });
 $("#gold-crystal").on("click", function () {
-    var goldVal = crystalVal[2];
-    alert("Your current score is = " + goldVal); ;
+    addValue(crystal.gold);
 });
 $("#green-crystal").on("click", function () {
-    var greenVal = crystalVal[3];
-    alert("Your current score is = " + greenVal); 
+    addValue(crystal.green);
 });
-
