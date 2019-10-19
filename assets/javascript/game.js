440f84e07b7ea1e64b619556f9f598a264da9f05
Alex Santillan <asantill68@yahoo.com>
2019-10-19 16:05:09 -0500

    If ElseIf statements is not working properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//Crystal Variables
+//Crystal Variables created in an object to facilitate value assignment
 var crystal = {
     red:
     {
@@ -25,22 +25,15 @@ var crystal = {
 //Scores to track
 var score   = 0;
 var target  = 0;
-//Generates random target between 19 and 120
-//var target = Math.floor(Math.random() * 101) + 19;
-//$("#target").html(target);
 
 //Create and reset variables for wins and losses.
 var wins    = 0;
 var losses  = 0;
 
-//Create an array and assign four values <19
-//Use a for-loop to randomely assign values between 1 and 19 to each spot on array
-//var crystalVal = [1, 3, 10, 15];
-
 //Functions
 
 //Reset the current score
-var score = 0;
+score = 0;
 
 //General random number generator with two limits
 var getRandom = function(min, max) {
@@ -60,14 +53,11 @@ crystal.green.value = getRandom (1, 12);
 //Change the html to refelect the changes
 $("#target").html(target);
 
-
-
-
 //test
-//console.log("-------------------------------------");
-//console.log("target score " + target);
-//console.log("red: " + crystal.red.value + " | blue : " + crystal.blue.value + " | gold: " + crystal.gold.value + " | green: " + crystal.green.value);
-//console.log ("-------------------------------------")
+console.log("-------------------------------------");
+console.log("target score " + target);
+console.log("red: " + crystal.red.value + " | blue : " + crystal.blue.value + " | gold: " + crystal.gold.value + " | green: " + crystal.green.value);
+console.log ("-------------------------------------")
 };
 //Starts the game
 startGame ();
@@ -75,14 +65,14 @@ startGame ();
 //Function to add crystal values
 var addValue = function(crystal){
     score = score + crystal.value;
-    //test
-    //console.log("Your score " + score);
 
     //Update the score
-    $("#score").html(score);
+    $("#score").html("Score: " + score);
 
     //Call checkWin
     checkWin();
+    //test
+    console.log("Your score " + score);
 }
 
 //Function to check Wins
@@ -93,16 +83,18 @@ var checkWin = function() {
         losses ++;
         //Update html
         $("#losses").html("Losses: " + losses);
-        }
-    else if(score == target) {
-    alert("Congratulations!!!  You Won");
-    //Update wins
-    wins ++;
-    //Update html
-    $("#wins").html("Wins: " + wins);
-    }
-};
-
+        //Restart game
+        startGame();
+    } else if(score == target) {
+        alert("Congratulations!!!  You Won");
+        //Update wins
+        wins ++;
+        //Update html
+        $("#wins").html("Wins: " + wins);
+        //Restart game
+        startGame();
+    };
+}
 //Assign on.Click and value to images
 $("#red-crystal").on("click", function () {
     addValue(crystal.red);
